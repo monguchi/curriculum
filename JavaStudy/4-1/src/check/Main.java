d03b1cfaec50fb5a7e0634c9b7bad0c796898b86
monguchi <102403554+monguchi@users.noreply.github.com>
2022-06-04 17:59:45 +0900

    あ
    あ

--- JavaStudy/4-1/src/check/Main.java
+++ JavaStudy/4-1/src/check/Main.java
@@ -4,21 +4,22 @@ import constants.Constants;
 
 public class Main {
 
-	private String firstName="門口";
-	private String lastName ="絵里菜";
+	private static String firstName="門口";
+	private static String lastName ="絵里菜";
 	
 	private static void printName(String firstName,String lastName) {
-		System.out.println("printNameメソッド→"+ printName(firstName, lastName));
+		System.out.println("printNameメソッド→"+ firstName+lastName);
 		
 	}
 	
 		public static void main(String[] args) {
 			
-			Pet pet = new Pet();
-			Constants constants = new Constants();
+			Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
+			RobotPet robotPet =new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
 			
-			
-		
+			printName(firstName,lastName);
+		    pet.introduce();
+		    pet.introduce();
 			
 		}
 		
