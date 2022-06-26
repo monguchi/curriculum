bddb5a6639aac9c164e6f96eb8c1ddebc11f286f
monguchi <102403554+monguchi@users.noreply.github.com>
2022-06-26 15:22:49 +0900

    課題
    質問7.8.9がわかりません

---
+++ JavaStudy/5-1/src/Main.java
@@ -0,0 +1,80 @@
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		{
+//      問① 下記の配列はString型しか格納できないように修正してください。
+//		   現状は全てのクラスの継承元であるObject型を指定しているので、Integerもintも格納可能になっています。
+	       List<String> array = new ArrayList<String>();
+//	    <以下記述>
+
+//      問② 前から二つ目の要素を"bar"にしましょう。
+//	　<以下記述>
+	       array.add("erina");
+	       array.add("bar");
+	       array.add("foo");
+	       
+//      問③ fooが格納されているインデックスを出力してください。
+//	　<以下記述>
+	       System.out.println("fooのインデックス:"+ array.indexOf("foo"));
+	       
+		}
+
+		{
+//      問④ キーがString型、バリューがObject型の要素を三つ格納しましょう。
+		Map<String, Object> map = new HashMap<>();
+//	　<以下記述>
+		map.put("adress", "////");
+		map.put("name", "erina");
+		map.put("age", 24);
+
+//      問⑤上記で格納したキーを繰り返し文で出力しましょう。
+//	    <以下記述>
+
+		for(Map.Entry<String, Object> entry : map.entrySet()) {
+			
+			System.out.println("key:"+ entry.getKey());
+		}
+		
+//      問⑥上記で格納したバリューを繰り返し文で出力しましょう。
+//	    <以下記述>
+		for(Object value : map.values()) {
+			System.out.println("value:"+value);
+			
+		}
+
+		}
+		{
+//      問⑦ 配列arrayDateに今の年・月・日を入れてください。
+           Calendar cal = Calendar.getInstance();
+          
+          
+//	    <以下記述>
+//      問⑧ 配列arrayDateをList型に変更しましょう。
+//	    <以下記述>
+           List<Object> arrayDate =new ArrayList<>();
+           arrayDate.add(2022);
+           arrayDate.add(6);
+           arrayDate.add(26);
+           
+//      問⑨ 上記で格納した要素を繰り返しで出力しましょう。
+//	    <以下記述>
+           
+           for(Object obj : arrayDate) {
+        	   System.err.println(arrayDate);
+        	   
+           }
+		}
+	}
+
+
+}
